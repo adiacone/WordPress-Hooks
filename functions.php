dfcd5724385498df625d4f1e91eec33572355e9b
Adrian Diaconescu <adrian.diaconescu@gmail.com>
2015-04-13 06:27:29 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+/**
+ * Makes the tags metabox appear on the page editing screen.
+ */
+function prefix_tags_for_pages() {
+	register_taxonomy_for_object_type( 'post_tag', 'page' );
+}
+add_action( 'init', 'prefix_tags_for_pages' );
